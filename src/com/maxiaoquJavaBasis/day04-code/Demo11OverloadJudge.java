aeaca4cef0c2426099e53bda948712df6c4e53da
maxiaoqu <maxiaoqu@163.com>
2020-04-26 13:12:50 +0800

    方法重载+方法重载练习

---
+++ src/com/maxiaoquJavaBasis/day04-code/Demo11OverloadJudge.java
@@ -0,0 +1,23 @@
+// 判断哪些是正常的重载关系
+public class Demo11OverloadJudge {
+    /*
+    // no1
+    public static void open(){} // 正确重载
+    // no2
+    public static void open(int a){} // 正确重载
+    // no3
+    public static void open(double a,int b){} // 正确重载
+
+
+    // no4
+    static void open(int a,int b){} // 代码错误：和第no8冲突
+    // no5
+    public static void open(int a,double b){} // 代码错误：和第no6冲突
+    // no6
+    public void open(int i,double d){} // 代码错误：和第no5冲突
+    // no7
+    public static void OPEN(){} // 代码正确不会报错，但是并不是有效重载
+    // no8
+    public static void open(int i,int j){} // 代码错误：和第no4冲突
+    */
+}
